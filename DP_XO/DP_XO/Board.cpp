f20e86102888184cf5bf8c381091e91369ab5460
rdmc10 <radu.m10@protonmail.com>
2023-02-24 19:39:20 +0200

    Added CheckBoardState function

--- DP_XO/DP_XO/Board.cpp
+++ DP_XO/DP_XO/Board.cpp
@@ -32,6 +32,72 @@ std::unordered_set<uint8_t> Board::GetAvailableIndices() const {
 	return m_availableIndices;
 }
 
+Board::BoardState Board::CheckGameState()
+{
+     // Check rows and columns
+    for (int i = 0; i < boardSize; i++) {
+        if ((m_board[i][0] != Sign::NONE) && (m_board[i][0] == m_board[i][1])) {
+            bool win = true;
+            for (int j = 1; j < boardSize; j++) {
+                if (m_board[i][j] != m_board[i][0]) {
+                    win = false;
+                    break;
+                }
+            }
+            if (win) {
+                return BoardState::Win;
+            }
+        }
+        if ((m_board[0][i] != Sign::NONE) && (m_board[0][i] == m_board[1][i])) {
+            bool win = true;
+            for (int j = 1; j < boardSize; j++) {
+                if (m_board[j][i] != m_board[0][i]) {
+                    win = false;
+                    break;
+                }
+            }
+            if (win) {
+                return BoardState::Win;
+            }
+        }
+    }
+
+    // Check diagonals
+    bool win = true;
+    for (int i = 1; i < boardSize; i++) {
+        if (m_board[i][i] != m_board[0][0] || m_board[i][i] == Sign::NONE) {
+            win = false;
+            break;
+        }
+    }
+    if (win && m_board[0][0] != Sign::NONE) {
+        return BoardState::Win;
+    }
+
+    win = true;
+    for (int i = 1; i < boardSize; i++) {
+        if (m_board[i][boardSize - i - 1] != m_board[0][boardSize - 1] || m_board[i][boardSize - i - 1] == Sign::NONE) {
+            win = false;
+            break;
+        }
+    }
+    if (win && m_board[0][boardSize - 1] != Sign::NONE) {
+        return BoardState::Win;
+    }
+
+    // Check if there are any empty cells left
+    for (int i = 0; i < boardSize; i++) {
+        for (int j = 0; j < boardSize; j++) {
+            if (m_board[i][j] == Sign::NONE) {
+                return BoardState::Unfinished;
+            }
+        }
+    }
+
+    // If we get here, the game is a tie
+    return BoardState::Draw;
+}
+
 void Board::ResetBoard()
 {
 	for (size_t i = 0; i < boardSize; ++i)
--- DP_XO/DP_XO/Board.h
+++ DP_XO/DP_XO/Board.h
@@ -10,10 +10,17 @@
 class Board
 {
 public:
+	enum class BoardState {
+		Win,
+		Draw,
+		Unfinished
+	};
+public:
 	Board();
 	void ResetBoard();
 	std::array<std::array<Sign, boardSize>, boardSize> GetBoard();
 	std::unordered_set<uint8_t> GetAvailableIndices() const;
+	BoardState CheckGameState();
 	bool setOption(uint32_t option, const Player& player);
 
 private:
